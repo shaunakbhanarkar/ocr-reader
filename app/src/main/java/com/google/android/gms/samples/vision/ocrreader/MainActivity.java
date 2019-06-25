c3295768f8051ae6bf37130a585f185b521f33ec
2016A7PS0016G <f20160016@goa.bits-pilani.ac.in>
2019-06-25 20:25:11 +0530

    Initial commit

---
+++ app/src/main/java/com/google/android/gms/samples/vision/ocrreader/MainActivity.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.gms.samples.vision.ocrreader;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+
+/**
+ * Main activity demonstrating how to pass extra parameters to an activity that
+ * recognizes text.
+ */
+public class MainActivity extends Activity implements View.OnClickListener {
+
+
+    String led1 = "OFF";
+    String led2 = "OFF";
+    String led3 = "OFF";
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+    final DatabaseReference ledstatus1 = myRef.child("led1").child("status");
+
+    final DatabaseReference ledstatus2 = myRef.child("led2").child("status");
+
+    final DatabaseReference ledstatus3 = myRef.child("led3").child("status");
+
+
+
+    // Use a compound button so either checkbox or switch widgets work.
+    private CompoundButton autoFocus;
+    private CompoundButton useFlash;
+    private TextView statusMessage;
+    private TextView textValue;
+
+    private static final int RC_OCR_CAPTURE = 9003;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        statusMessage = (TextView)findViewById(R.id.status_message);
+        textValue = (TextView)findViewById(R.id.text_value);
+
+        autoFocus = (CompoundButton) findViewById(R.id.auto_focus);
+        useFlash = (CompoundButton) findViewById(R.id.use_flash);
+
+        findViewById(R.id.read_text).setOnClickListener(this);
+
+        ledstatus1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                led1 = value;
+                Log.d("file", "Value is: " + value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+        ledstatus2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                led2 = value;
+                Log.d("file", "Value is: " + value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+        ledstatus3.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                led3 = value;
+                Log.d("file", "Value is: " + value);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("file", "Failed to read value.", error.toException());
+            }
+        });
+
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.read_text) {
+            // launch Ocr capture activity.
+            Intent intent = new Intent(this, OcrCaptureActivity.class);
+            intent.putExtra(OcrCaptureActivity.AutoFocus, autoFocus.isChecked());
+            intent.putExtra(OcrCaptureActivity.UseFlash, useFlash.isChecked());
+
+            startActivityForResult(intent, RC_OCR_CAPTURE);
+        }
+    }
+
+    /**
+     * Called when an activity you launched exits, giving you the requestCode
+     * you started it with, the resultCode it returned, and any additional
+     * data from it.  The <var>resultCode</var> will be
+     * {@link #RESULT_CANCELED} if the activity explicitly returned that,
+     * didn't return any result, or crashed during its operation.
+     * <p/>
+     * <p>You will receive this call immediately before onResume() when your
+     * activity is re-starting.
+     * <p/>
+     *
+     * @param requestCode The integer request code originally supplied to
+     *                    startActivityForResult(), allowing you to identify who this
+     *                    result came from.
+     * @param resultCode  The integer result code returned by the child activity
+     *                    through its setResult().
+     * @param data        An Intent, which can return result data to the caller
+     *                    (various data can be attached to Intent "extras").
+     * @see #startActivityForResult
+     * @see #createPendingResult
+     * @see #setResult(int)
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == RC_OCR_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    String text = data.getStringExtra(OcrCaptureActivity.TextBlockObject);
+//                    statusMessage.setText(R.string.ocr_success);
+//                    textValue.setText(text + led1 + led2 + led3);
+                    if (text.contains("led1"))
+                    {
+                        if (led1.equals("OFF"))
+                            ledstatus1.setValue("ON");
+                        else if (led1.equals("ON"))
+                            ledstatus1.setValue("OFF");
+                    }
+                    else if (text.contains("led2"))
+                    {
+                        if (led2.equals("OFF"))
+                            ledstatus2.setValue("ON");
+                        else if (led2.equals("ON"))
+                            ledstatus2.setValue("OFF");
+                    }
+                    else if (text.contains("led3"))
+                    {
+                        if (led3.equals("OFF"))
+                            ledstatus3.setValue("ON");
+                        else if (led3.equals("ON"))
+                            ledstatus3.setValue("OFF");
+                    }
+//                    textValue.setText(text + led1 + led2 + led3);
+                    Log.d(TAG, "Text read: " + text);
+                } else {
+                    statusMessage.setText(R.string.ocr_failure);
+                    Log.d(TAG, "No Text captured, intent data is null");
+                }
+            } else {
+                statusMessage.setText(String.format(getString(R.string.ocr_error),
+                        CommonStatusCodes.getStatusCodeString(resultCode)));
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
